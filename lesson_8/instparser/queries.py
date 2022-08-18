d55a502dc131315c7bb3d4eba8efdcf456b87700
Damir <damir.urmanov2003@mail.ru>
2022-08-19 03:47:48 +0500

    commented queries file + added requirements

--- lesson_8/instparser/queries.py
+++ lesson_8/instparser/queries.py
@@ -1,13 +1,22 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
+#
+# client = MongoClient("localhost", 27017)
+# db = client["instparser"]
+# inst = db.instagram
+# user_in_db = input("Enter the user that exists in the database: ")
+#
+# i = 1
+# for user in inst.find(
+#     {
+#         "$and": [
+#             {f"{user_in_db}.status": "following"},
+#             {f"{user_in_db}.status": {"$ne": "follower"}},
+#         ]
+#     }
+# ):
+#     print(user[user_in_db]["username"], i)
+#     i += 1
+#     print(user[user_in_db]["username"], user[user_in_db]["status"])
 
-client = MongoClient("localhost", 27017)
-db = client["instparser"]
-inst = db.instagram
-user_in_db = input("Enter the user that exists in the database: ")
-
-
-for user in inst.find({f"{user_in_db}.status": "follower"}):
-    print(user[user_in_db]["username"], user[user_in_db]["status"])
-
-for user in inst.find({f"{user_in_db}.status": "following"}):
-    print(user[user_in_db]["link"], user[user_in_db]["status"])
+# for user in inst.find({f"{user_in_db}.status": "following"}):
+#     print(user[user_in_db]["link"], user[user_in_db]["status"])
